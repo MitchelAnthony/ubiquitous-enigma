22d4e6130f497b7cca191bea4f32806bed333750
Mitchel Vroege <19216221+MitchelAnthony@users.noreply.github.com>
2018-11-16 23:53:53 +0100

    Solved problem 2

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,27 @@ pub mod solution {
     
         sum
     }
+
+    pub fn two() -> u32 {
+        let max = 4000000;
+        let mut first = 1;
+        let mut second = 2;
+        let mut sum = 0;
+
+        while second < max {
+            if second % 2 == 0 {
+                sum += second;
+            }
+
+            let new = first + second;
+            first = second;
+            second = new;
+        }
+
+        println!("The sum of all the even-valued terms is {}", sum);
+
+        sum
+    }
 }
 
 #[cfg(test)]
@@ -22,5 +43,10 @@ mod tests {
     #[test]
     fn test_one() {
         assert_eq!(solution::one(), 233168);
+    }
+
+    #[test]
+    fn test_two() {
+        assert_eq!(solution::two(), 4613732);
     }
 }
